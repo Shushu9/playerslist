c40dcd2da44c015a82e209e9410572ab11138ca7
Shushu9 <pokotilo.gleb1@gmail.com>
2023-04-11 18:30:19 +0300

    feat: done task

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,13 @@
-
 import ParticipantsList from '../participants-list/participants-list';
+
 
 const App = () => {
 
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>Пары / Результаты</h2>
-      <h3 style={{ textAlign: "center" }}> Тур 2023 / 03 / 12</h3 >
-
+      <h2 style={{ textAlign: "center", margin: 0 }}>Пары / Результаты</h2>
+      <h3 style={{ textAlign: "center", margin: 0 }}> Тур 2023 / 03 / 12</h3 >
       <ParticipantsList />
-
     </div >
   );
 }
--- src/components/participants-list/participants-list.js
+++ src/components/participants-list/participants-list.js
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react";
-import './participants-list.css';
+// import * as React from 'react';
+// import { styled } from '@mui/styles';
+// import './participants-list.css';
+import { makeStyles } from '@material-ui/styles';
+
+
+
+
+
+
+
 
 
 const ParticipantsList = () => {
 
     const [data, setData] = useState([]);
 
+    ///******gettind players list  */
     const fetchData = () => {
         fetch(`./participants.json`)
             .then((response) => response.json())
             .then((actualData) => {
-                console.log(actualData);
+                // console.log(actualData);
                 setData(actualData);
             })
             .catch((err) => {
@@ -22,8 +33,16 @@ const ParticipantsList = () => {
         fetchData();
     }, []);
 
+
+
+
+    //********classes ********* */
+
+    const classes = styles();
+
+
     // ******* condition that chose how much tables should we render
-    const makeTwoTables = (data.length > 50 && window.innerWidth >= 1200) ? true : false;
+    const makeTwoTables = (data.length > 20 && window.innerWidth >= 1200) ? true : false;
 
 
     // ******* creates list of participants pairs
@@ -33,15 +52,15 @@ const ParticipantsList = () => {
 
         for (let i = initNumber; i < lastNumber; i = i + 2) {
             content.push(
-                <tr key={pairNumber - 1} className="table__participants">
-                    <td className="table__center">{pairNumber}</td>
-                    <td className="table__name">{data[i].name}</td>
-                    <td className="table__center">{data[i].rating}</td>
-                    <td className="table__center">0</td>
-                    <td className="table__center">0 - 0</td>
-                    <td className="table__center">0</td>
-                    <td className="table__name">{data[i + 1].name}</td>
-                    <td className="table__center">{data[i + 1].rating}</td>
+                <tr key={pairNumber - 1} className={classes.participants}>
+                    <td className={classes.tdCenter}>{pairNumber}</td>
+                    <td className={classes.tdName}>{data[i].name}</td>
+                    <td className={classes.tdCenter}>{data[i].rating}</td>
+                    <td className={classes.tdCenter}>0</td>
+                    <td className={classes.tdCenter}>0 - 0</td>
+                    <td className={classes.tdCenter}>0</td>
+                    <td className={classes.tdName}>{data[i + 1].name}</td>
+                    <td className={classes.tdCenter}>{data[i + 1].rating}</td>
                 </tr>
             );
             pairNumber++;
@@ -52,22 +71,21 @@ const ParticipantsList = () => {
 
     // ******* creates header of participants table
     const makeTableHead = (data, initNumber, lastNumber) => {
-
         const table = (
-            <table className="table">
-                <thead className="table__header">
-                    <tr>
-                        <td className="table__center">№</td>
-                        <td className="table__name">Имя</td>
-                        <td className="table__center">Рейт</td>
-                        <td className="table__center">Очки</td>
-                        <td className="table__center">Результат</td>
-                        <td className="table__center">Очки</td>
-                        <td className="table__name">Имя</td>
-                        <td className="table__center">Рейт</td>
+            <table className={classes.table}>
+                <thead>
+                    <tr className={classes.tdHeader}>
+                        <td className={classes.tdCenter} >№</td>
+                        <td className={classes.tdName}>Имя</td>
+                        <td className={classes.tdCenter}>Рейт</td>
+                        <td className={classes.tdCenter}>Очки</td>
+                        <td className={classes.tdCenter}>Результат</td>
+                        <td className={classes.tdCenter}>Очки</td>
+                        <td className={classes.tdName}>Имя</td>
+                        <td className={classes.tdCenter} nter>Рейт</td>
                     </tr>
                 </thead>
-                <tbody className="table__body">
+                <tbody>
                     {makeParticipantsList(data, initNumber, lastNumber)}
                 </tbody>
             </table>
@@ -82,7 +100,7 @@ const ParticipantsList = () => {
     }
 
     return (
-        <div className="pairs">
+        <div className={classes.tableWrapper}>
             {makeTwoTables ? makeTableHead(data, 0, Math.ceil(data.length / 2)) : makeTableHead(data, 0, data.length - 1)}
             {makeTwoTables ? makeTableHead(data, Math.ceil(data.length / 2) + 1, data.length - 1) : null}
         </div>
@@ -92,3 +110,68 @@ const ParticipantsList = () => {
 export default ParticipantsList;
 
 
+
+const styles = makeStyles({
+    tableWrapper: {
+        fontFamily: "'Inter', sans-serif",
+        fontWeight: '400',
+        fontSize: '0.8em',
+        display: 'flex',
+        justifyContent: 'space-around',
+        height: 'calc(100vh - 60px)',
+    },
+    '@media (max-width: 560px)': {
+        tableWrapper: {
+            fontSize: '0.6em',
+        }
+    },
+    table: {
+        margin: '5px',
+        borderSpacing: '0',
+        borderRadius: '5px',
+        border: '1px solid rgba(224, 224, 224, 1)',
+    },
+    tdCenter: {
+        textAlign: 'center',
+    },
+    tdName: {
+        maxWidth: '175px',
+    },
+
+    tdHeader: {
+        '& td': {
+            padding: '10px',
+            fontWeight: 'bold',
+            color: '#fff',
+            backgroundColor: 'rgba(0, 0, 0, 1)',
+        },
+
+        '& td:first-child': {
+            borderTopLeftRadius: '5px',
+        },
+
+        '& td:last-child': {
+            borderTopRightRadius: '5px',
+        }
+    },
+
+    '@media (max-width: 600px)': {
+        tdHeader: {
+            '& td': {
+                padding: '10px 5px',
+            }
+        }
+
+    },
+
+    participants: {
+        '& td': {
+            padding: '10px',
+            borderTop: '1px solid rgba(224, 224, 224, 1)',
+        },
+        '&:nth-child(odd)': {
+            backgroundColor: 'rgba(0, 0, 0, 0.04)',
+        }
+    },
+
+});
